c7a702760e2366e424d8f938c7d6957a4c9f850f
Sumit23062005 <sumitjune123@gmail.com>
2026-02-11 00:40:17 +0530

    finishing up project

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react" ;
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const productList = ["Market", "ERC20 Token", "Donation"] ;
+  const contractList = [
+    "support@dreamfund.com",
+    "info@example.com",
+    "Contact Us",
 
-export default Footer
+  ];
+
+  const usefullLink = ["Home", "About Us", "Company Bio"] ;
+  return (
+    <footer className ="text-center text-white backgroundMain lg:text-left">
+      <div className="mx-6 py-10 text-center md:text-left">
+        <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+            <div className="">
+              <h6 className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
+                DreamFund
+              </h6>
+              <p>
+                Here you can use rows and colums to organize your footer content.
+                Lorem ipsum dolor sit amet consectetur adipisicing.
+              </p>
+
+            </div>
+            <div className="">
+              <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+                  Products
+              </h6>
+                {productList.map((el , i) => (
+                  <p className="mb-4" key={i+1}>
+                    <a href="#!">{el}</a>
+                  </p>
+                ))}
+            </div>
+            <div className="">
+              <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+                 Usefull links
+              </h6>
+              {usefullLink.map((el,i) => (
+                <p className="mb-4" key={i+1}>
+                  <a href="#!">{el}</a>
+                </p>
+              ))}
+
+            </div>
+            <div>
+              <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+                   Start
+              </h6>
+              {contractList.map((el,i) => (
+                <p className="mb-4" key={i+1}>
+                  <a href="#!">{el}</a>
+                </p>
+              ))}
+            </div>
+        </div>      
+        </div>
+        <div className="backgroundMain p-6 text-center">
+          <span>
+            © 2026 copyright:
+          </span>
+          <a className="font-semibold" href="https://tailwind-elements.com/">
+            DreamFund
+          </a>
+          
+        </div>
+    </footer>
+  );
+};
+
+export default Footer ;
--- src/Components/index.js
+++ src/Components/index.js
@@ -2,3 +2,6 @@ export { default as NavBar } from "./NavBar";
 export { default as Footer } from "./Footer";
 export { default as Logo } from "./Logo";
 export { default as Menu } from "./Menu";
+export { default as Hero } from "./Hero";
+export { default as Card } from "./Card";
+export { default as PopUp } from "./PopUp";
